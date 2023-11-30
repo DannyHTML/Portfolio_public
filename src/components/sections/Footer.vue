b7b3331cb76a06cc5042ec21768a46fd7253de56
Danny <dannyjag@gmail.com>
2023-11-30 14:50:52 +0100

    Added container class

--- src/components/sections/Footer.vue
+++ src/components/sections/Footer.vue
@@ -1,33 +1,35 @@
 <template>
-  <div class="mt-20 flex h-20 items-center justify-around bg-black">
-    <div class="w-16 text-center">
-      <a href="https://www.github.com/DannyHTML" target="_blank">
-        <font-awesome-icon
-          class="text-primary"
-          :icon="['fab', 'square-github']"
-          size="2xl"
-        />
-      </a>
-    </div>
-    <div class="text-center text-white">
-      <div>
-        <p class="mb-[2px] text-sm">
-          {{ language.translatedText.footer.title }}
-        </p>
-        <a href="https://github.com/DannyHTML" target="_blank"
-          ><p class="-rotate-3">
-            <span
-              class="uppercase text-primary underline underline-offset-4 md:font-medium"
-              >{{ language.translatedText.footer.author }}</span
-            >
-          </p>
+  <div class="bg-black">
+    <div class="mt-20 container mx-auto flex h-20 items-center justify-around">
+      <div class="w-16 text-center">
+        <a href="https://www.github.com/DannyHTML" target="_blank">
+          <font-awesome-icon
+            class="text-primary"
+            :icon="['fab', 'square-github']"
+            size="2xl"
+          />
         </a>
       </div>
-    </div>
-    <div class="w-16 text-right text-lg">
-      <p class="ml text-white">
-        <span class="mr-1 inline">&copy;</span>{{ currentYear }}
-      </p>
+      <div class="text-center text-white">
+        <div>
+          <p class="mb-[2px] text-sm">
+            {{ language.translatedText.footer.title }}
+          </p>
+          <a href="https://github.com/DannyHTML" target="_blank"
+            ><p class="-rotate-3">
+              <span
+                class="uppercase text-primary underline underline-offset-4 md:font-medium"
+                >{{ language.translatedText.footer.author }}</span
+              >
+            </p>
+          </a>
+        </div>
+      </div>
+      <div class="w-16 text-right text-lg">
+        <p class="ml text-white">
+          <span class="mr-1 inline">&copy;</span>{{ currentYear }}
+        </p>
+      </div>
     </div>
   </div>
 </template>
