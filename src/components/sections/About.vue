a8ffe65f545a848cabb20d7e8a6dae0444578745
Danny <dannyjag@gmail.com>
2023-11-30 14:38:37 +0100

    Added container class

--- src/components/sections/About.vue
+++ src/components/sections/About.vue
@@ -1,28 +1,30 @@
 <template>
-  <div>
-    <h2 id="About" class="mb-10 mt-16 custom-title">
-      {{ language.translatedText.about.title }}
-    </h2>
-  </div>
-  <div class="flex justify-center items-center w-full">
-    <div
-      class="flex flex-col lg:flex-row items-center justify-around w-full px-5"
-    >
-      <img
-        class="w-96 rounded-md shadow-xl"
-        src="/images/danny.jpg"
-        alt="Image of Danny Jager"
-      />
-      <div>
-        <div class="text-center max-w-xl">
-          <p class="mt-5 border-colour rounded-md p-4 shadow-lg">
-            {{ language.translatedText.about.paragraph_1 }}
-          </p>
-        </div>
-        <div class="text-center max-w-xl">
-          <p class="mt-5 border-colour rounded-md p-4 shadow-lg">
-            {{ language.translatedText.about.paragraph_2 }}
-          </p>
+  <div class=" xl:container xl:mx-auto">
+    <div>
+      <h2 id="About" class="mb-10 mt-16 custom-title">
+        {{ language.translatedText.about.title }}
+      </h2>
+    </div>
+    <div class="flex justify-center items-center w-full">
+      <div
+        class="flex flex-col lg:flex-row items-center justify-around w-full px-5"
+      >
+        <img
+          class="w-96 rounded-md shadow-xl"
+          src="/images/danny.jpg"
+          alt="Image of Danny Jager"
+        />
+        <div>
+          <div class="text-center max-w-xl">
+            <p class="mt-5 border-colour rounded-md p-4 shadow-lg">
+              {{ language.translatedText.about.paragraph_1 }}
+            </p>
+          </div>
+          <div class="text-center max-w-xl">
+            <p class="mt-5 border-colour rounded-md p-4 shadow-lg">
+              {{ language.translatedText.about.paragraph_2 }}
+            </p>
+          </div>
         </div>
       </div>
     </div>
