3aab33646c9c6a2a0184e2f23d8bda4910fd34f5
Danny <dannyjag@gmail.com>
2023-11-30 14:41:17 +0100

    Added container class

--- src/components/sections/Projects.vue
+++ src/components/sections/Projects.vue
@@ -1,53 +1,55 @@
 <template>
-  <div class="mb-5">
-    <div class="mb-12 pt-20">
-      <h2 id="Projects" class="custom-title">
-        {{ language.translatedText.projects.title }}
-      </h2>
-    </div>
-    <div class="mx-5 flex flex-wrap justify-center gap-5">
-      <div
-        v-for="project in projects"
-        :key="project.id"
-        class="relative border-colour mx-auto my-5 rounded-md p-4 shadow-xl"
-      >
-        <div class="flex flex-col items-center justify-center">
-          <h3 class="mb-4 text-center text-2xl font-bold lg:text-3xl">
-            {{ project.title }}
-          </h3>
-          <a :href="project.urlLive" target="_blank"
-            ><img
-              class="rounded-lg shadow-[0px_5px_15px] shadow-gray-400 transition-all hover:scale-105 sm:max-w-sm"
-              :src="project.img"
-              alt="Example of project for mobile, tablet and desktop. Responsive design."
-          /></a>
-        </div>
+  <div class="container mx-auto">
+    <div class="mb-5">
+      <div class="mb-12 pt-20">
+        <h2 id="Projects" class="custom-title">
+          {{ language.translatedText.projects.title }}
+        </h2>
+      </div>
+      <div class="mx-5 flex flex-wrap justify-center gap-5">
         <div
-          class="mx-auto mb-20 flex items-center px-2 pt-8 text-center sm:max-w-sm"
+          v-for="project in projects"
+          :key="project.id"
+          class="relative border-colour mx-auto my-5 rounded-md p-4 shadow-xl"
         >
-          <p>
-            {{ project.description }}
-          </p>
-        </div>
-        <div class="absolute bottom-0 left-0 mb-5 flex w-full justify-around">
-          <div>
-            <a :href="project.urlLive" target="_blank">
-              <CustomButton :icon="['fas', 'arrow-up-right-from-square']" 
-                >website</CustomButton
-              ></a
-            >
+          <div class="flex flex-col items-center justify-center">
+            <h3 class="mb-4 text-center text-2xl font-bold lg:text-3xl">
+              {{ project.title }}
+            </h3>
+            <a :href="project.urlLive" target="_blank"
+              ><img
+                class="rounded-lg shadow-[0px_5px_15px] shadow-gray-400 transition-all hover:scale-105 sm:max-w-sm"
+                :src="project.img"
+                alt="Example of project for mobile, tablet and desktop. Responsive design."
+            /></a>
           </div>
-          <div>
-            <a :href="project.urlGithub" target="_blank"
-              ><CustomButton :icon="['fab', 'square-github']" size="lg"
-                >github</CustomButton
-              ></a
-            >
+          <div
+            class="mx-auto mb-20 flex items-center px-2 pt-8 text-center sm:max-w-sm"
+          >
+            <p>
+              {{ project.description }}
+            </p>
+          </div>
+          <div class="absolute bottom-0 left-0 mb-5 flex w-full justify-around">
+            <div>
+              <a :href="project.urlLive" target="_blank">
+                <CustomButton :icon="['fas', 'arrow-up-right-from-square']" 
+                  >website</CustomButton
+                ></a
+              >
+            </div>
+            <div>
+              <a :href="project.urlGithub" target="_blank"
+                ><CustomButton :icon="['fab', 'square-github']" size="lg"
+                  >github</CustomButton
+                ></a
+              >
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
+ </div>
 </template>
 
 <script setup lang="ts">
